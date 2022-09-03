761bbb23ef69d270d8d237cad379d4b2867cb02a
Ashley-23 <ashleysatchivi92@gmail.com>
2022-09-03 16:06:41 +0000

    Dernier commit

---
+++ .history/local/moodlepe/index_20220821183031.php
@@ -0,0 +1,332 @@
+<?php
+
+
+if (isset($_GET['suppr'])) {
+
+    $suppression = $_GET['suppr'];
+
+    if ($suppression == "ok") {
+        echo "<script> alert('La suppression a été effectué avec succès') </script>";
+    }
+}
+
+
+if (isset($_GET['userexist'])) {
+
+    $userexist = $_GET['userexist'];
+
+    if ($userexist == "ok") {
+        echo "<script> alert('Cet utilisateur est un administrateur') </script>";
+    }
+}
+
+
+
+if (isset($_GET['emailoruser'])) {
+
+    $emailoruser = $_GET['emailoruser'];
+
+    if ($emailoruser == "ok") {
+        echo "<script> alert('Cet utilisateur existe déjà') </script>";
+    }
+}
+
+
+
+if (isset($_GET['reset'])) {
+
+    $reset = $_GET['reset'];
+
+    if ($reset == "ok") {
+        // echo "<script> alert('Le mot de passe a été réinitialisé avec succès') </script>";
+
+        echo "<script> ";
+        if (isset($_GET['rest']) && ($_GET['subproject'] == "no")) {
+            swal("Le mot de passe a été réinitialisé avec succès", " echo $error_not_eligible_submission_project", "error");
+        }
+        echo "</script>";
+    }
+}
+
+
+if (isset($_GET['mod'])) {
+
+    $modification = $_GET['mod'];
+
+    if ($modification == "ok") {
+        echo "<script> alert('La modification a été effectué avec succès') </script>";
+    }
+}
+
+if (isset($_GET['create'])) {
+
+    $modification = $_GET['create'];
+
+    if ($modification == "ok") {
+        echo "<script> alert('La création a été effectué avec succès') </script>";
+    }
+}
+
+if (isset($_GET['supprerreur'])) {
+
+    $supprerreur = $_GET['supprerreur'];
+
+    if ($supprerreur == "ok") {
+        echo "<script> alert('Il y a eu une erreur dans la suppression') </script>";
+    }
+}
+
+
+if (isset($_GET['moderreur'])) {
+
+    $moderreur = $_GET['moderreur'];
+
+    if ($moderreur == "ok") {
+        echo "<script> alert('Il y a eu une erreur dans la modification') </script>";
+    }
+}
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Plugin strings are defined here.
+ * 
+ * @package     local_moodlepe
+ * @category    string
+ * @copyright   2022 SATCHIVI Kokoè Yasmine Ashley <ashleysatchivi92@gmail.com>
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+require_once('../../config.php');
+require_once($CFG->dirroot . '/local/moodlepe/edit_database_name.php');
+
+require_login();
+
+if (isguestuser()) {
+    throw new moodle_exception('noguest');
+}
+
+// RECUPERER L'ID DE L'UTILISATEUR CONNECTE
+$userid = $USER->id;
+// echo $userid;
+
+//CONNEXION AVEC MA BASE DE DONNEE
+//OUVERTURE
+$dbhostname = database_hostname();
+
+$dbname =  database_name();
+
+$dbusername = database_username();
+
+$dbpassword = database_password();
+
+$dbport = database_port();
+
+$mysql = new mysqli($dbhostname, $dbusername, $dbpassword, $dbname, $dbport);
+
+// RECUPERER L'ID DE NOTRE ROLE 
+
+$role_name = "Administrateur pour les entreprises";
+$role_shortname = "administrateur_entreprise";
+$role_description = "role du plugin moodlepe";
+$role_archetype = "manager";
+
+$check_role_id = $mysql->query("SELECT * FROM mdl_role WHERE name='$role_name' AND shortname='$role_shortname' AND description ='$role_description'", MYSQLI_USE_RESULT);
+
+while ($row = mysqli_fetch_assoc($check_role_id)) {
+    $verify_role_id = $row['id'];
+}
+
+// echo "l'id de notre role est : " . $verify_role_id;
+
+// RECUPERER L'ID DU ROLE DE L'UTILISATEUR CONNECTE
+
+
+
+$role_check = $mysql->query("SELECT r.id,r.name, a.userid FROM mdl_role r JOIN mdl_role_assignments a ON r.id = a.roleid WHERE a.userid='$userid' AND a.roleid='$verify_role_id'", MYSQLI_USE_RESULT);
+
+while ($row = $role_check->fetch_assoc()) {
+    $roleid =  $row['id'];
+}
+
+if (isset($roleid)) {
+
+
+
+    if ($roleid == $verify_role_id) {
+        // C'EST ICI QUE TOUT SE PASSE
+
+        $context = context_system::instance();
+        $PAGE->set_context($context);
+        $PAGE->set_url(new moodle_url('/local/moodlepe/index.php'));
+        $PAGE->set_pagelayout('standard');
+        $PAGE->set_title($SITE->fullname);
+        $PAGE->set_heading(get_string('name', 'local_moodlepe'));
+
+        echo $OUTPUT->header();
+
+        // // RECUPERER L'ID DE L'UTILISATEUR CONNECTE
+        // $userid = $USER->id;
+
+
+        // RECUPERER L'ID DE LA COHORTE DE L'UTILISATEUR CONNECTE
+
+        $cohort_check = $mysql->query("SELECT c.id FROM mdl_cohort c JOIN mdl_cohort_members m ON c.id = m.cohortid WHERE m.userid = '$userid'", MYSQLI_USE_RESULT);
+
+        while ($row = $cohort_check->fetch_assoc()) {
+            $cohortid =  $row['id'];
+        }
+
+        // RECUPERER LA LISTE D'UTILISATEURS 
+        // ****************************************************************************************************************************************************************************************************
+        $userlist = $mysql->query("SELECT u.id, u.firstname ,u.lastname ,u.username, u.email,c.name  FROM mdl_cohort c JOIN mdl_cohort_members m  ON c.id = m.cohortid JOIN mdl_user u  ON u.id = m.userid WHERE m.cohortid='$cohortid'AND u.id!='$userid' ", MYSQLI_USE_RESULT);
+
+
+        $users = array();
+        while ($row = $userlist->fetch_assoc()) {
+            $users[] = array('id' => $row['id'], 'nom' => $row['lastname'], 'prenom' => $row['firstname'], 'username' => $row['username'], 'email' => $row['email'], 'entreprise' => $row['name']);
+        }
+
+?>
+
+
+
+
+        <!-- -->
+
+        <?php ?>
+
+        <!-- *********************************** AJOUTER UN UTILISATEUR****************************************** -->
+        <form action="/moodlepe/local/moodlepe/user/add.php">
+            <div class="col-md-9 form-inline align-items-start felement" data-fieldtype="group" id="yui_3_17_2_1_1660412984554_753">
+                <fieldset class="w-100 m-0 p-0 border-0" id="yui_3_17_2_1_1660412984554_752">
+                    <div class="d-flex flex-wrap align-items-center" id="yui_3_17_2_1_1660412984554_751">
+
+                        <div class="form-group  fitem  " id="yui_3_17_2_1_1660412984554_750">
+                            <span data-fieldtype="submit" id="yui_3_17_2_1_1660412984554_749">
+                                <input type="submit" class="btn
+                        btn-primary
+                        
+                    
+                    " id="id_submitbutton" value="Ajouter un utilisateur" data-initial-value="Ajouter un utilisateur">
+                            </span>
+                            <div class="form-control-feedback invalid-feedback" id="id_error_submitbutton">
+
+                            </div>
+                        </div>
+
+                </fieldset>
+                <div class="form-control-feedback invalid-feedback" id="fgroup_id_error_buttonar">
+
+                </div>
+            </div>
+        </form>
+        <!-- *********************************** FIN AJOUTER UN UTILISATEUR****************************************** -->
+
+
+
+
+
+        <div class="no-overflow">
+            <div class="table-responsive">
+                <table class="admintable generaltable table-sm" id="users">
+                    <thead>
+                        <tr>
+                            <th class="header c0 centeralign" style="" scope="col"> Prénom / Nom</th>
+                            <th class="header c1 centeralign" style="" scope="col"> Adresse de courriel </th>
+                            <th class="header c4" style="" scope="col"> Nom d'utilisateur</th>
+                            <th class="header c4" style="" scope="col"> Entreprise</th>
+                            <th class="header c5" style="" scope="col"> Modifier</th>
+                            <td class="header c6 lastcol" style=""></td>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                        <?php
+                        foreach ($users as $user) {
+                            echo "<tr class=''>";
+                            echo "<td class='centeralign cell c0' style=''>" . $user['nom'] . ' ' . $user['prenom'] . "</td>";
+                            echo "<td class='centeralign cell c1' style=''>" . $user['email'] . "</td>";
+                            echo "<td class='centeralign cell c1' style=''>" . $user['username'] . "</td>";
+                            echo "<td class='centeralign cell c1' style=''>" . $user['entreprise'] . "</td>";
+                            // echo "<td class='cell c4' style=''>maintenant</td>";
+                        ?>
+                            <td class="cell c5" style="" id="yui_3_17_2_1_1660560784812_579">
+                                <!-- <input type="submit" style="color:blue" onclick="getid('<?php echo $user['username'] ?>');" value="Modifier"> -->
+                                <!--  -->
+
+                                <a class="icon fa fa-cog fa-fw " title="Modifier" role="img" aria-label="Modifier" onclick="getid('<?php echo $user['username'] ?>');"></a>
+                                <!--  -->
+                                <a class="icon fa fa-eye fa-fw " onclick="resetpassword('<?php echo $user['username'] ?>');" title="Réinitialiser le mot de passe" role="img" aria-label="Réinitialiser le mot de passe"></a>
+                                <!--  -->
+                                <a href="" onclick="check('<?php echo $user['username'] ?>');"><i class=' icon fa fa-trash fa-fw' title='Supprimer' role='img' aria-label='Supprimer'></i></a>
+
+                            </td>
+
+                        <?php
+                            echo "</tr>";
+                        }
+                        ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
+        </table>
+
+        <script type="text/javascript">
+            var check = function(username) {
+                if (confirm('Voulez-vous vraiment supprimer l\'utilisateur « ' + username + ' » ?')) {
+                    window.location.href = "/moodlepe/local/moodlepe/user/delete.php?username=" + username;
+                } else {
+                    return false;
+                };
+            }
+
+
+            var getid = function(username) {
+                // return id;
+                window.location.href = "/moodlepe/local/moodlepe/user/update.php?username=" + username;
+            };
+
+
+            var resetpassword = function(username) {
+                if (confirm('Voulez-vous vraiment réinitialiser le mot de passe de l\'utilisateur « ' + username + ' » ?')) {
+                    window.location.href = "/moodlepe/local/moodlepe/user/resetpassword.php?username=" + username;
+                } else {
+                    return false;
+                };
+            }
+        </script>
+        <!-- </form> -->
+
+<?php
+        echo $OUTPUT->footer();
+
+        // TERMINUS
+    } else {
+        //ON LE REDIRIGE
+        echo "1 , mon role est : " . $roleid;
+        // header("Location: /moodlepe/index.php");
+        die();
+    }
+} else {
+    // ON LE REDIRIGE
+
+    header("Location: /moodlepe/index.php");
+    die();
+}
+
+die();
---
+++ .history/local/moodlepe/user/createadd_20220818121554.php
@@ -0,0 +1,262 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Plugin strings are defined here.
+ * 
+ * @package     local_moodlepe
+ * @category    string
+ * @copyright   2022 SATCHIVI Kokoè Yasmine Ashley <ashleysatchivi92@gmail.com>
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+require_once('../../../config.php');
+require_once($CFG->dirroot . '/local/moodlepe/edit_database_name.php');
+
+
+require_once($CFG->dirroot . '/local/moodlepe/lib.php');
+require_once($CFG->dirroot . '/local/moodlepe/lang/en/local_moodlepe.php');
+require_once($CFG->dirroot . '/local/moodlepe/message_form.php');
+
+require_login();
+
+if (isguestuser()) {
+    throw new moodle_exception('noguest');
+}
+
+//CONNEXION AVEC MA BASE DE DONNEE
+//OUVERTURE
+
+$dbhostname = database_hostname();
+
+$dbname =  database_name();
+
+$dbusername = database_username();
+
+$dbpassword = database_password();
+
+$dbport = database_port();
+
+$mysql = new mysqli($dbhostname, $dbusername, $dbpassword, $dbname, $dbport);
+
+$authuserid = $USER->id;
+
+$timecreated = time();
+
+
+
+$auth = 'manual';
+$confirmed = 1;
+$deleted = 0;
+$timezone = '99';
+$mnethostid = 1;
+$lang = 'fr';
+$timecreated = time();
+$timemodified = time();
+
+
+
+
+if (isset($_POST['username']) || isset($_POST['lastname']) || isset($_POST['firstname']) || isset($_POST['email']) || isset($_POST['entreprise']) || isset($_POST['password']) || isset($_POST['entrepriseid'])) {
+    $firstname = $_POST['firstname'];
+    $lastname = $_POST['lastname'];
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $entreprise = $_POST['entreprise'];
+    $mdp = $_POST['password'];
+    $password = hash_internal_user_password($mdp);
+    $entrepriseid = $_POST['entrepriseid'];
+
+    // $user = array();
+    // $user[] = array(
+    //     'firstname' => $firstname,
+    //     'lastname' => $lastname,
+    //     'username' => $username,
+    //     'email' => $email,
+    //     'password' => $password,
+    //     'entreprise' => $entreprise
+    // );
+
+    // print_r($user);
+    // echo "</br>";
+
+
+    // REQUETES POUR RECUPERER LE CHAMP VOULU DANS LA BASE DE DONNEE S'IL COORESPOND A LA SAISIE DE L'UTILISATEUR
+    $check_user_username = $mysql->query("SELECT * FROM mdl_user WHERE UPPER(username)=UPPER('$username')");
+    $check_user_firstname = $mysql->query("SELECT * FROM mdl_user WHERE UPPER(firstname)=UPPER('$firstname')");
+    $check_user_lastname = $mysql->query("SELECT * FROM mdl_user WHERE UPPER(lastname)=UPPER('$lastname')");
+    $check_user_email = $mysql->query("SELECT * FROM mdl_user WHERE UPPER(email)=UPPER('$email')");
+
+    // WHERE UPPER(LastName) = UPPER('AnGel')
+    // VERIFICATION DE L'EXISTANCE DU NOM D'UTILISATEUR DE L'UTILISATEUR
+    $row1 = $check_user_username->fetch_array(MYSQLI_ASSOC);
+    $verify_user_username = $row1["username"];
+    if (empty($verify_user_username)) {
+        $username_result = 0;
+    } else {
+        // if ($verify_user_username == $username) {
+        if (strcasecmp($verify_user_username, $username) == 0) {
+            $username_result = 1;
+        } else {
+            $username_result = 0;
+        }
+    }
+
+    // VERIFICATION DE L'EXISTANCE DU PRENOM DE L'UTILISATEUR
+    $row2 = $check_user_firstname->fetch_array(MYSQLI_ASSOC);
+    $verify_user_firstname = $row2["firstname"];
+
+    if (empty($verify_user_firstname)) {
+        $firstname_result = 0;
+    } else {
+        // if ($verify_user_firstname == $firstname) {
+        if (strcasecmp($verify_user_firstname, $firstname) == 0) {
+            $firstname_result = 1;
+        } else {
+            $firstname_result = 0;
+        }
+    }
+
+    // VERIFICATION DE L'EXISTANCE DU NOM DE FAMILLE DE L'UTILISATEUR
+    $row3 = $check_user_lastname->fetch_array(MYSQLI_ASSOC);
+    $verify_user_lastname = $row3["lastname"];
+    if (empty($verify_user_lastname)) {
+        $lastname_result = 0;
+    } else {
+        // if ($verify_user_lastname == $lastname) {
+        if (strcasecmp($verify_user_lastname, $lastname) == 0) {
+            $lastname_result = 1;
+        } else {
+            $lastname_result = 0;
+        }
+    }
+
+
+    // VERIFICATION DE L'EXISTANCE DE L'EMAIL DE L'UTILISATEUR
+    $row4 = $check_user_email->fetch_array(MYSQLI_ASSOC);
+    $verify_user_email = $row4["email"];
+
+    if (empty($verify_user_email)) {
+        $email_result = 0;
+    } else {
+        // if ($verify_user_email == $email) {
+        if (strcasecmp($verify_user_email, $email) == 0) {
+            $email_result = 1;
+        } else {
+            $email_result = 0;
+        }
+    }
+
+
+    // ***************************************************** VERIFICATIONS *************************************************************
+    if ($lastname_result == 1 || $email_result == 1) {
+
+
+        //RECUPERE L'ID DE L'UTILISATEUR SAISIE
+        $check_user_id_1 = $mysql->query("SELECT * FROM mdl_user WHERE UPPER(email)=UPPER('$email')", MYSQLI_USE_RESULT);
+        while ($row = mysqli_fetch_assoc($check_user_id_1)) {
+            $user_id = $row['id'];
+        }
+
+        // ON VERIFIE S'IL EST DEJA MEMBRE DE NOTRE SOCIETE
+        $check_cohort_member = $mysql->query("SELECT * FROM mdl_cohort_members WHERE cohortid='$entrepriseid' AND userid='$user_id'", MYSQLI_USE_RESULT);
+
+        // S'IL NE L'EST PAS, ON L'AJOUTE DANS LA SOCIETE
+        // if ($check_cohort_member == 'NULL') {
+        if (empty($check_cohort_member)) {
+
+            echo "you";
+            // IL EXISTE DONC ON RECUPERE SON ID ET ON L'INSCRIT DANS NOTRE ENTREPRISE
+
+            //CREE UNE NOUVELLE INSTANCE DE COHORT_MEMBERS 
+
+            // $mdl_cohort_members  =  "INSERT INTO mdl_cohort_members (id,cohortid, userid, timeadded) VALUES ('','$entrepriseid', '$user_id','$timecreated')";
+            // $mysql->query($mdl_cohort_members);
+            // ***************************************$check_me = $mysql->query("SELECT * FROM mdl_user WHERE UPPER(email)=UPPER('$email')", MYSQLI_USE_RESULT);
+            // while ($row = mysqli_fetch_assoc($check_user_id_1)) {
+            //     $user_id = $row['id'];
+            // }
+
+            // ***************************************sizeof($check_me);
+            // ***************************************die();
+
+
+            // $mdl_cohort_members  = $mysql->query("INSERT INTO mdl_cohort_members (id,cohortid, userid, timeadded) VALUES ('','$entrepriseid', '$user_id','$timecreated')", MYSQLI_USE_RESULT);
+
+            // *************************************************************
+            // $mdl_cohort_members  = $mysql->query("INSERT INTO mdl_cohort_members SET cohortid = '$entrepriseid', userid = '$user_id', timeadded = '$timecreated') ", MYSQLI_USE_RESULT);
+            // *************************************************************
+
+            // echo $mdl_cohort_members;
+            // die();
+
+            // // echo $mdl_cohort_members;
+            // ENREGISTRE L'UTILISATEUR DANS LA TABLE DE NOTRE PLUGIN
+
+            // 
+            // $mdl_local_moodlepe_suppr  = "INSERT INTO mdl_local_moodlepe_suppr (id,username, firstname, lastname,email ,password ,enterprise ,timecreated ,active ,suppr ,suppr_user_id ,userid)VALUES ('','$username', '$firstname', '$lastname','$email', '$password', '$entreprise','$timecreated', 't','f', '$authuserid', '$user_id')";
+            // $mysql->query($mdl_local_moodlepe_suppr);
+
+            // *************************************************************
+            // $mdl_local_moodlepe_suppr  = $mysql->query("INSERT INTO mdl_local_moodlepe_suppr SET username = '$username', firstname = '$firstname', lastname =  '$lastname',email  = '$email',password  = '$password',enterprise =  '$entreprise',timecreated = '$timecreated' ,active = 't' ,suppr = 'f',suppr_user_id  = '$authuserid',userid = '$user_id')", MYSQLI_USE_RESULT);
+            // *************************************************************
+            // ON LE RETOURNE SUR LA PAGE AVEC LA LISTE D'UTILISATEURS
+            // *************************************************************
+            // header("Location: /moodlepe/local/moodlepe/index.php?create=ok");
+            // *************************************************************
+            // 
+            die();
+        }
+        // 
+
+        // ON LE RETOURNE SUR LA PAGE AVEC LA LISTE D'UTILISATEURS
+        // header("Location: /moodlepe/local/moodlepe/index.php");
+        // die();
+    } else {
+
+        echo "3";
+        // S'IL N'EXISTE PAS, ON LE CRE ET ON LE MET DANS LA COHORTE DE L'ADMINISTRATEUR D'ENTREPRISE
+
+        // $mdl_user  =
+        $mysql->query("INSERT INTO mdl_user ( auth, confirmed, deleted, timezone,  username, firstname, lastname,mnethostid, lang,  password, email, timecreated, timemodified ) VALUES ('$auth', '$confirmed', '$deleted','$timezone','$username', '$firstname', '$lastname','$mnethostid', '$lang', '$password', '$email',  '$timecreated', '$timemodified')", MYSQLI_USE_RESULT);
+
+        //RECUPERE L'ID DE L'UTILISATEUR QU'IL VIENT DE CREER 
+        $check_user_id = $mysql->query("SELECT * FROM mdl_user WHERE email='$email'", MYSQLI_USE_RESULT);
+
+        while ($row = mysqli_fetch_assoc($check_user_id)) {
+            $verify_user_id = $row['id'];
+        }
+
+        //CREE UNE NOUVELLE INSTANCE DE COHORT_MEMBERS 
+        $mdl_cohort_members  = $mysql->query("INSERT INTO mdl_cohort_members (cohortid, userid, timeadded ) VALUES ('$entrepriseid', '$verify_user_id','$timecreated')", MYSQLI_USE_RESULT);
+
+        // ENREGISTRE L'UTILISATEUR DANS LA TABLE DE NOTRE PLUGIN
+        //   $mdl_local_moodlepe_suppr  =
+        $mysql->query("INSERT INTO mdl_local_moodlepe_suppr (username, firstname, lastname,email ,password ,enterprise ,timecreated ,active ,suppr ,suppr_user_id ,userid) VALUES ('$username', '$firstname', '$lastname','$email', '$password', '$entreprise','$timecreated', 't','f', '$authuserid', '$verify_user_id')", MYSQLI_USE_RESULT);
+
+
+        // ON LE RETOURNE SUR LA PAGE AVEC LA LISTE D'UTILISATEURS
+        header("Location: /moodlepe/local/moodlepe/index.php");
+        die();
+    }
+} else {
+
+    // ON LE RETOURNE SUR LA PAGE AVEC LA LISTE D'UTILISATEURS
+    header("Location: /moodlepe/local/moodlepe/index.php");
+    die();
+}
+
+mysqli_close($mysql);
